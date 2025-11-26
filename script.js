5e7f63347e3108f89c42c318104acf8efef6d796
Nicolas Lameiras Navarro <nicolaslameiras@gmail.com>
2025-11-26 17:55:07 -0300

    Add commented-out code for loops and conditions

---
+++ script.js
@@ -0,0 +1,34 @@
+// for (let i = 1;i<=20;i++){
+
+//     if( i%3 == 0){
+
+//         continue
+
+//     }
+//     else if ( i == 16){
+
+//         break
+
+//     }
+//     console.log(i)
+// }
+// let i = 0
+// while(i <= 10){
+// i++
+//   console.log(i)
+// }
+
+// let i = 0
+// while( i <= 15){
+// i++;
+
+// if( i == 8){
+//     continue
+// };
+
+// if( i == 12){
+//     break
+// }
+
+// console.log(i)
+// }
